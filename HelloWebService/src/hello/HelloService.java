c131d16b0169b5f78dd921300c4723a65b46a160
cgtyozdmr <cgtyozdmrr@gmail.com>
2017-08-30 12:19:37 +0300

    @Path ile metotlara çeşitli parametreler verilip nümerik veya non-nümerik olarak sunucuda bu parametreler ile kaynağın görüntülenmesi sağlandı.

---
+++ HelloWebService/src/hello/HelloService.java
@@ -0,0 +1,20 @@
+package hello;
+
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/helloWorld")
+public class HelloService {
+	@GET
+	@Path("{name}")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String hello(@PathParam("name") String name) {
+		
+		return "Merhaba "+ name;
+	}
+
+}
